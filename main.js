d556e26d626ce4bb8f5ce2ebf023deec32b7b39b
Oliver Davey <oliver.davey@callvision-tech.com>
2016-05-16 22:18:26 +0100

    Added initial testing request to police data for current neighbourhood

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
+var lat = 0;
+var lng = 0;
+
 function initMap()
 {
 	//Get user's current location and focus map
 	navigator.geolocation.getCurrentPosition(function(position) {
+		
+		lat = position.coords.latitude;
+		lng = position.coords.longitude;
+		
 		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: position.coords.latitude, lng: position.coords.longitude},
+			center: {lat: lat, lng: lng},
 			zoom: 13
 		});
 	
 	});
 
-	
+	$.get('https://data.police.uk/api/locate-neighbourhood',
+		{q : lat+','+lng}
+	)
+	.done( function(data){
+		console.log(data);
+		//drawBoundaryFromCoords()
+	})
 }
