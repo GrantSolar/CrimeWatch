09cf272063f59c7a8378a69602306421ab410b1c
Oliver Davey <oliver.davey@callvision-tech.com>
2016-05-18 19:50:29 +0100

    drawRadius makes new circle

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ var area;
 
 function drawRadius(lat, lng, radius)
 {
-	area.center = {lat : lat, lng : lng};
-	area.radius = radius;
+	area = new google.maps.Circle({ strokeColor : '#0000FF',
+			strokeOpacity : 0.8,
+			strokeWeight: 2,
+			fillColor : '#0000FF',
+			fillOpacity : 0.35,
+			map : map,
+			center : { lat : lat, lng : lng},
+			radius : radius});
 }
 
 function initMap()
