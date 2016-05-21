21c169bc7299755161e5e22190529577371d8abb
Oliver Davey <oliver.davey@callvision-tech.com>
2016-05-21 09:32:38 +0100

    reduced opacity of circle

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function changeRadius(lat, lng, radius)
 			strokeOpacity : 0.8,
 			strokeWeight: 2,
 			fillColor : '#0000FF',
-			fillOpacity : 0.35,
+			fillOpacity : 0.2,
 			map : map,
 			center : { lat : lat, lng : lng},
 			radius : radius});
